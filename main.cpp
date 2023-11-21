bc2c785096d30a1bdde9406adbce2f45fbba7f86
Parhelion <93296510+chappi17@users.noreply.github.com>
2023-11-21 12:38:56 +0900

    Createing a blank canvas & Set background Color.

--- main.cpp
+++ main.cpp
@@ -3,38 +3,21 @@
 
 using namespace std;
 
+Color green = {173, 204,96,255};
+Color darkGreen = {43,51,24,255};
+
 int main () {
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
+    const int screenWidth = 750;
+    const int screenHeight = 750;
 
-    cout << "Hello World" << endl;
-
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(screenWidth, screenHeight, "Retro Snake Game");
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false){
         BeginDrawing();
-        ClearBackground(BLACK);
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
+        ClearBackground(green);
 
-        if(ball_x + ball_radius >= screenWidth  || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
-
-        if(ball_y + ball_radius >= screenHeight  || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-
-        DrawCircle(ball_x,ball_y,ball_radius, WHITE);
         EndDrawing();
     }
 
